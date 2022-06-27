8e4f68aa2966b4fc95bb674a6e34a472e450696d
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-06-27 23:18:37 +0200

    -

--- code/main.py
+++ code/main.py
@@ -5,8 +5,6 @@ characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 def generate_random_password():
 	length = int(input("Enter password length: "))
 	random.shuffle(characters)
-	
-	## picking random characters from the list
 	password = []
 	for i in range(length):
 		password.append(random.choice(characters))
