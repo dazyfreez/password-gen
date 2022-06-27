bbf49483c2af0f09d5f8ee8bf9ab5f4eadce2704
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-06-27 23:30:07 +0200

    -

--- code/main.py
+++ code/main.py
@@ -28,8 +28,4 @@ def generate_random_password():
 			password.append(random.choice(characters))
 	random.shuffle(password)
 	print("".join(password))
-
-
-
-## invoking the function
 generate_random_password()
